eeaa2fd706423a0013201fe544e2d638d99e84d9
RedsTom <tom.frefre.13@gmail.com>
2022-12-30 19:36:01 +0100

    Update and rename main.py to main.java

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+  public static void main(String... args) {
+    System.out.println("Ma branche master est super stable 2023"); 
+  }
+  
+}
--- main.py
+++
@@ -1,1 +0,0 @@
-  print("Ma branche master est super stable 2023")
