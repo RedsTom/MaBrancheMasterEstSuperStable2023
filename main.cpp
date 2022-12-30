7ac03d17840226344ed123af5ccd882965460aa3
Pixfri <jean.letessier@protonmail.com>
2022-12-30 23:11:23 +0100

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+        std::cout << "Ma branche est extrêmement plus stable que celle de RedsTom mais en plus c'est l'édition Professional." << std::endl;
+
+        return 0;
+}
